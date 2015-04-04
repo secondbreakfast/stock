2342eae6455ba144537d6720ff72b13aa48388de
w <w@lxv.io>
2015-04-03 19:32:11 -0500

    Initial Commit

---
+++ Stock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stock
+//
+//  Created by Will Schreiber on 4/3/15.
+//  Copyright (c) 2015 LXV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stock
+//
+//  Created by Will Schreiber on 4/3/15.
+//  Copyright (c) 2015 LXV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
